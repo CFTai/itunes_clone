8e719c05cfc10c172a09cc4a04fda4c3450d9338
Stephen Tai <stephentai@fundpark.com>
2021-04-01 10:23:27 +0800

    Init  project

---
+++ itunesClone/itunesClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  itunesClone
+//
+//  Created by stephen tai on 1/4/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
